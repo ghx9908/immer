b468a2ad59b157483cd178cd530a296dd43cf1a1
“hongxiang.gao” <hongxiang.gao@retech.work>
2023-02-28 14:45:25 +0800

    feat: 基本使用

--- src/index.js
+++ src/index.js
@@ -1,8 +1,14 @@
-import { produce } from "immer"
-let baseState = { name: "zs" }
+// import { produce } from "immer"
+// let baseState = {
+//   ids: [1],
+//   pos: {
+//     x: 1,
+//     y: 1,
+//   },
+// }
 
-let nextState = produce(baseState, (draft) => {
-  draft.name = "ls"
-})
-console.log(baseState === nextState)
-console.log("nextState=>", nextState)
+// let nextState = produce(baseState, (draft) => {
+//   draft.ids.push(2)
+// })
+// console.log(baseState.ids === nextState.ids) //false
+// console.log(baseState.pos === nextState.pos) //true
